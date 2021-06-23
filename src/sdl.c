e41e24be2f2e2874850dd0ff630fba3cd3503498
flamingowrangler2869 <flamingowrangler2869@gmail.com>
2021-06-23 00:14:56 -0400

    setup map system

--- src/sdl.c
+++ src/sdl.c
@@ -26,22 +26,22 @@ const int cx = w/2;
 const int cy = h/2;
 
 const char map[16][16] = {
-    {B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B},
-    {B,0,0,0,W,0,0,0,0,0,0,0,0,0,0,B},
-    {B,0,0,0,0,0,0,0,0,0,0,0,0,0,0,B},
-    {B,0,0,0,W,0,0,0,0,0,0,0,0,0,0,B},
-    {B,0,0,0,W,0,0,0,0,0,0,0,0,0,0,B},
-    {B,0,0,0,W,0,0,W,0,0,0,0,0,0,0,B},
-    {B,0,0,0,W,0,0,0,0,0,0,0,0,0,0,B},
-    {B,0,0,0,W,0,0,0,0,0,0,W,0,0,0,B},
-    {B,0,0,0,W,0,0,0,0,0,0,0,0,0,0,B},
-    {B,0,0,0,W,0,0,0,0,0,0,0,0,0,0,B},
-    {B,0,0,0,W,0,0,0,0,0,0,0,0,0,0,B},
-    {B,0,0,0,W,0,0,0,0,0,W,0,0,0,0,B},
-    {B,0,0,0,W,0,0,0,0,0,0,0,0,0,0,B},
-    {B,0,0,0,W,0,0,0,0,0,0,0,0,0,0,B},
-    {B,0,0,0,0,0,0,0,0,0,0,0,0,0,0,B},
-    {B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B}
+    {W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W},
+    {W,0,0,0,W,0,0,0,0,0,0,0,0,0,0,W},
+    {W,0,0,0,0,0,0,0,0,0,0,0,0,0,0,W},
+    {W,0,0,0,W,0,0,0,0,0,0,0,0,0,0,W},
+    {W,0,0,0,W,0,0,0,0,0,0,0,0,0,0,W},
+    {W,0,0,0,W,0,0,W,0,0,0,0,0,0,0,W},
+    {W,0,0,0,W,0,0,0,0,0,0,0,0,0,0,W},
+    {W,0,0,0,W,0,0,0,0,0,0,W,0,0,0,W},
+    {W,0,0,0,W,0,0,0,0,0,0,0,0,0,0,W},
+    {W,0,0,0,W,0,0,0,0,0,0,0,0,0,0,W},
+    {W,0,0,0,W,0,0,0,0,0,0,0,0,0,0,W},
+    {W,0,0,0,W,0,0,0,0,0,W,0,0,0,0,W},
+    {W,0,0,0,W,0,0,0,0,0,0,0,0,0,0,W},
+    {W,0,0,0,W,0,0,0,0,0,0,0,0,0,0,W},
+    {W,0,0,0,0,0,0,0,0,0,0,0,0,0,0,W},
+    {W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W}
 };
 
 int main() {
@@ -60,48 +60,39 @@ int main() {
 			if (event.type == SDL_QUIT) running = false;
 			
 			// keyboard controls
-			// switch(event.type) {
-			// 	case SDL_KEYDOWN:
-					if (event.key.keysym.sym == SDLK_ESCAPE) return 0; // close on escape
-					if (event.key.keysym.sym == SDLK_w && plrY != 0)	plrY -= rh; // top border
-					if (event.key.keysym.sym == SDLK_s && plrY != h-rh)	plrY += rh; // bottom border
-					if (event.key.keysym.sym == SDLK_a && plrX != 0)	plrX -= rw; // left border
-					if (event.key.keysym.sym == SDLK_d && plrX != w-rw)	plrX += rw; // right border
-					// break;
-			// }
+			if (event.type == SDL_KEYDOWN) {
+				if (event.key.keysym.sym == SDLK_ESCAPE) return 0; // close on escape
+				if (event.key.keysym.sym == SDLK_w && plrY != 0)	plrY -= rh; // up
+				if (event.key.keysym.sym == SDLK_s && plrY != h-rh)	plrY += rh; // down
+				if (event.key.keysym.sym == SDLK_a && plrX != 0)	plrX -= rw; // left
+				if (event.key.keysym.sym == SDLK_d && plrX != w-rw)	plrX += rw; // right
+			}
 		}
 
 		set_draw_color(0, 0, 0); // set background color
 		render_clear();
 	
 		{
-			set_draw_color(236, 207, 55); // set player color
-			fill_rect(plrX, plrY, plrW, plrH); // draw player
-
-			// boundary limits
-			if (plrX < 0) plrX = 0;
-			if (plrX > w-rw) plrX = w - rw;
-			if (plrY < 0) plrY = 0;
-			if (plrY > h-rh) plrY = h - rh;
-
-
 			for (int x = 0; x < mw; x++) {
 				for (int y = 0; y < mh; y++) {
 					switch (map[x][y]) {
 						case W:
-							// SDL_Rect wall = {rect_size*y, rect_size*x, rw, rh}; // setup wall
-							set_draw_color(86, 98, 246); // set wall color
-							// SDL_RenderFillRect(renderer, &wall); // draw wall
-						case B:
-							set_draw_color(255, 0, 0);
+							set_draw_color(86, 98, 246); // wall color
+							break;
+						default:
+							set_draw_color(0, 0, 0);
+							break;
 					}
+					fill_rect(rect_size*y, rect_size*x, rw, rh);
 				}
 			}
+
+			set_draw_color(236, 207, 55); // set player color
+			fill_rect(plrX, plrY, plrW, plrH); // draw player
 		}
 
-		// display the things
-		render_present();
+		render_present(); // display the things
 	}
 
-
+	destroyGFX(); // clean up sdl
 }
