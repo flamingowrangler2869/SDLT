5247238d6daafc87b75b4208068bbd960947d65e
flamingowrangler2869 <flamingowrangler2869@gmail.com>
2021-06-23 14:08:12 -0400

    migrate globals to config.h

---
+++ src/config.h
@@ -0,0 +1,28 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+extern SDL_Event event;
+
+// window width and height
+#define WIN_WIDTH  800
+#define WIN_HEIGHT 800
+
+// centers of the window
+#define cx WIN_WIDTH/2
+#define cy WIN_HEIGHT/2
+
+// map objects
+#define W 1		// wall
+#define B 2		// B tile
+#define P -1	// player start
+
+// map size
+#define mw WIN_WIDTH/rw
+#define mh WIN_HEIGHT/rh
+
+// tile size
+#define RECT_SIZE 50
+#define rw RECT_SIZE
+#define rh RECT_SIZE
+
+#endif /* CONFIG_H */
--- src/sdl.c
+++ src/sdl.c
@@ -7,24 +7,7 @@
 
 #include "utils.h"
 #include "gfx.h"
-
-#define uchar unsigned char
-#define W 1
-#define B 2
-#define P -1
-
-const int w = 800;
-const int h = 800;
-
-const int rect_size = 50;
-const int rw = rect_size;
-const int rh = rect_size;
-
-const int mw = w/rw;
-const int mh = h/rh;
-
-const int cx = w/2;
-const int cy = h/2;
+#include "config.h"
 
 const char map[16][16] = {
     {W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W},
@@ -41,26 +24,30 @@ const char map[16][16] = {
     {W,0,0,0,0,0,0,0,0,0,0,0,0,0,0,W},
     {W,0,0,0,0,0,0,0,0,0,0,0,0,0,0,W},
     {W,0,0,0,0,0,0,0,0,0,0,0,0,0,0,W},
-    {W,0,0,0,0,0,0,0,0,0,0,0,0,0,0,W},
+    {W,B,0,0,0,0,0,0,0,0,0,0,0,0,0,W},
     {W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W}
 };
 
 int main() {
+
+	initGFX("window", WIN_WIDTH, WIN_HEIGHT);
+
+	// if P is on the map, the player
+	// starts there; otherwise,
+	// it starts in the middle
 	int plrW = rw;
 	int plrH = rh;
 	int plrX = cx;
 	int plrY = cy;
-	initGFX("window", w, h);
 	for (int x = 0; x < mw; x++) {
 		for (int y = 0; y < mh; y++) {
 			if (map[x][y] == P) {
-				plrX = rect_size*x;
-				plrY = rect_size*y;
+				plrX = rw*x;
+				plrY = rh*y;
 			}
 		}
 	}
 
-	SDL_Event event;
 	bool running = true;
 	while (running) {
 		while (SDL_PollEvent(&event)) {
@@ -69,16 +56,16 @@ int main() {
 			// keyboard controls
 			if (event.type == SDL_KEYDOWN) {
 				if (event.key.keysym.sym == SDLK_ESCAPE) return 0; // close on escape
-				if (event.key.keysym.sym == SDLK_w && plrY != 0)	plrY -= rh; // up
-				if (event.key.keysym.sym == SDLK_s && plrY != h-rh)	plrY += rh; // down
-				if (event.key.keysym.sym == SDLK_a && plrX != 0)	plrX -= rw; // left
-				if (event.key.keysym.sym == SDLK_d && plrX != w-rw)	plrX += rw; // right
+				if (event.key.keysym.sym == SDLK_w && plrY != 0)				plrY -= rh; // up
+				if (event.key.keysym.sym == SDLK_s && plrY != WIN_HEIGHT-rh)	plrY += rh; // down
+				if (event.key.keysym.sym == SDLK_a && plrX != 0)				plrX -= rw; // left
+				if (event.key.keysym.sym == SDLK_d && plrX != WIN_WIDTH-rw)		plrX += rw; // right
 			}
 		}
 
 		set_draw_color(0, 0, 0); // set background color
 		render_clear();
-	
+
 		{
 			for (int x = 0; x < mw; x++) {
 				for (int y = 0; y < mh; y++) {
@@ -90,10 +77,10 @@ int main() {
 							set_draw_color(0xFF, 0, 0); // B tile color
 							break;
 						default:
-							set_draw_color(0, 0, 0);
+							set_draw_color(0, 0, 0); // background color
 							break;
 					}
-					fill_rect(rect_size*y, rect_size*x, rw, rh);
+					fill_rect(rh*y, rw*x, rw, rh);
 				}
 			}
 
